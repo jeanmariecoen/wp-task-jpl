b4c3ad54b115802ae147d5975f06a0fc17191f24
David Lelievre <projectionartt@gmail.com>
2019-06-10 12:10:45 -0400

    INSERT

--- wp-content/themes/twentynineteen/archive.php
+++ wp-content/themes/twentynineteen/archive.php
@@ -28,6 +28,8 @@ get_header();
 			while ( have_posts() ) :
 				the_post();
 
+				
+
 				/*
 				 * Include the Post-Format-specific template for the content.
 				 * If you want to override this in a child theme, then include a file
